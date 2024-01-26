6c6ff20739e74d359a034b0c2e985d8d7ea10bd4
David Carboni <david@carboni.io>
2024-01-26 16:03:38 +0000

    Slack Lambda

---
+++ slack/src/lambda.ts
@@ -0,0 +1,38 @@
+import {
+  Context, SQSBatchResponse, SQSEvent,
+} from 'aws-lambda';
+import axios from 'axios';
+
+/**
+ * Process the content of an SQS message
+ */
+export async function processMessage(body: string) {
+  const slackWebhook = process.env.SLACK_WEBHOOK || '';
+  if (slackWebhook) {
+    await axios.post(slackWebhook, { text: `${body}` });
+  }
+}
+
+/**
+ * Lambda handler to process a batch of SQS messages
+ */
+export async function handler(event: SQSEvent): Promise<SQSBatchResponse> {
+  // Process incoming message(s)
+  // and note any failures
+  const failedIds: string[] = [];
+  const records = event.Records.map(async (record) => {
+    try {
+      await processMessage(record.body);
+    } catch (err) {
+      failedIds.push(record.messageId);
+      console.error(`Message error: ${err} [${record.messageId}]`);
+    }
+  });
+  await Promise.all(records);
+
+  // Report on any failred items for retry
+  const result: SQSBatchResponse = {
+    batchItemFailures: failedIds.map((id) => ({ itemIdentifier: id })),
+  };
+  return result;
+}
