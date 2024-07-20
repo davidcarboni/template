77c396d253d50e483722cd6b56a913339c5e6a18
David Carboni <david@carboni.io>
2024-07-20 19:57:53 +0100

    Updates cdk.json to use tsx

---
+++ setup/cdkJson.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+
+const cdk = JSON.parse(fs.readFileSync('cdk.json').toString('utf8'));
+
+// Update "npx ts-node --prefer-ts-exts bin/<stack>.ts"
+// to "npx tsx bin/<stack>.ts"
+const app = cdk.app;
+const segments = app.split(' ');
+
+// Take the firsat and last segments and put 'tsx' in the middle
+cdk.app = `${segments.shift()} tsx ${segments.pop()}`;
+
+fs.writeFileSync('v.json', JSON.stringify(cdk, null, 2));
