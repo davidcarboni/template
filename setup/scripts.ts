3322853e2f04ed2e9c747bf64db335e7bf4b10bb
David Carboni <david@carboni.io>
2024-07-20 19:58:50 +0100

    "type":"module" and use tsx instead to ts-node

--- setup/scripts.ts
+++ setup/scripts.ts
@@ -2,8 +2,11 @@ import * as fs from 'fs';
 
 const pkg = JSON.parse(fs.readFileSync('package.json').toString('utf8'));
 
+// Update to ESM
+pkg.type = 'module';
+
 // Add additional scripts
 pkg.scripts.lint = "eslint --fix bin lib";
-pkg.scripts.secrets = "ts-node github.ts";
+pkg.scripts.secrets = "tsx github.ts";
 
 fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
