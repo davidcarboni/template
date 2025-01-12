e5ef68b21f7ea5095f1d76ed1bd8bff21d597b6b
David Carboni <david@carboni.io>
2025-01-12 16:11:20 +0000

    Don't remove any options set ny CDK

--- setup/tsconfigJson.ts
+++ setup/tsconfigJson.ts
@@ -4,7 +4,5 @@ const config = JSON.parse(fs.readFileSync('tsconfig.json').toString('utf8'));
 
 // Updates for ESM
 config.extends = '@tsconfig/node18/tsconfig.json';
-delete config.compilerOptions.module;
-delete config.compilerOptions.moduleResolution;
 
 fs.writeFileSync('tsconfig.json', JSON.stringify(config, null, 2));
