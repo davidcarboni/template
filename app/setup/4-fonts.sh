774ec9c13eacc4c098680bfc9979f436a5c8eca5
David Carboni <david@carboni.io>
2025-01-18 16:05:57 +0000

    Better documentation on getting postscriptname

--- app/setup/4-fonts.sh
+++ app/setup/4-fonts.sh
@@ -11,7 +11,7 @@ echo " - use a font in Expo Go (useFonts hook): https://docs.expo.dev/develop/us
 echo " - use Google fonts: https://docs.expo.dev/develop/user-interface/fonts/#use-google-fonts"
 echo " - bundle a font in a development build: https://docs.expo.dev/develop/user-interface/fonts/#use-a-local-font-file"
 echo "To check if a font is loaded, e.g. after bundling, use isLoaded('<Postscript name>');"
-echo " - get the Postscript name of a font in Linux, use fc-scan [TTF-font] - https://stackoverflow.com/a/68036706/723506"
+echo " - get the Postscript name of a font in Linux, use 'fc-scan [TTF-font] | grep postscriptname' - see: https://stackoverflow.com/a/68036706/723506"
 echo " - to handle variable fonts, see https://medium.com/timeless/adding-custom-variable-fonts-in-react-native-47e0d062bcfc and https://slice-gui.netlify.app/"
 
 # Optionally set up Slice for variable-width fonts
