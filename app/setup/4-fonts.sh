f7a9951e1565d9b51f9c0da172c7682d96542610
David Carboni <david@carboni.io>
2025-01-18 17:41:29 +0000

    Note on what to set in Slice.

--- app/setup/4-fonts.sh
+++ app/setup/4-fonts.sh
@@ -13,6 +13,7 @@ echo " - bundle a font in a development build: https://docs.expo.dev/develop/use
 echo "To check if a font is loaded, e.g. after bundling, use isLoaded('<Postscript name>');"
 echo " - get the Postscript name of a font in Linux, use 'fc-scan [TTF-font] | grep postscriptname' - see: https://stackoverflow.com/a/68036706/723506"
 echo " - to handle variable fonts, see https://medium.com/timeless/adding-custom-variable-fonts-in-react-native-47e0d062bcfc and https://slice-gui.netlify.app/"
+echo " - If using slice, set font weight in the top section and the postscript name in the bottom section, plus update a couple of other values where it may still say 'regular' when you're creating a bold slice. Not sure what the bold bit flags do."
 
 # Optionally set up Slice for variable-width fonts
 
