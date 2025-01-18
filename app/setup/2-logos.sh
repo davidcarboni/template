0b16708b4645b5bbd1533aa2064b450c9614cf3d
David Carboni <david@carboni.io>
2025-01-18 14:41:59 +0000

    Update on creating splash-icon.png

--- app/setup/2-logos.sh
+++ app/setup/2-logos.sh
@@ -9,7 +9,7 @@ xdg-open "https://www.figma.com/@expo"
 # echo "Play store icon:"
 # xdg-open https://www.figma.com/file/kHZexuf2L9nVxDRO6EOv5i/playstore_icon_template
 
-echo "Export the icon, adaptive-icon and splash as PNGs and save them to the assets folder:"
+echo "Export the icon and adaptive-icon as PNGs (splash is no longer needed - it can be generated from the icon) and save them to the assets folder:"
 echo " - assets/images/icon.png"
 echo " - assets/images/adaptive-icon.png"
-echo " - assets/images/splash.png"
+echo " - cp assets/images/icon.png assets/images/splash-ison.png"
