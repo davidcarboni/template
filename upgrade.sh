9ab7489d3a6607e1c4561b788ea919d8eb49620a
David Carboni <david@carboni.io>
2025-01-24 20:15:11 +0000

    Upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+root=$(pwd)
+
+# App dependencies
+cd ${root}/app
+yarn upgrade:expo
+
+# Lambdas
+for lambda in api slack
+do
+    cd ${root}/${lambda}
+    rm yarn.lock
+    yarn
+done
+
+# Global dependencies
+npm i -g \
+  npm \
+  yarn \
+  typescript \
+  ts-node \
+  aws-cdk \
+  react-native \
+  react-devtools \
+  create-react-native-app \
+  eas-cli \
+  --
+
+# Check the app code
+cd ${root}/app
+yarn lint
+yarn tsc
