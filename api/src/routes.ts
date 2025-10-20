7fd16b33671dc726150045457b4012a834400322
David Carboni <david@carboni.io>
2025-10-20 22:15:36 +0100

    Updatge to match shared

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,14 @@
 
-import { Routes } from '@scloud/lambda-api/dist/types';
+import { Route } from '@scloud/lambda-api/dist/types';
+import { paths } from '@shared/routes';
 import { ping } from './routes/ping';
 
-const routes: Routes = {
-  '/api/ping': { GET: { handler: ping } },
+export const routes: { [P in keyof typeof paths]: Route } = {
+  '/ping': { GET: { handler: ping } },
+  '/auth/apple': { POST: { handler: async () => ({ body: 'ok' }) } },
+  '/auth/google': { POST: { handler: async () => ({ body: 'ok' }) } },
+  '/user': { GET: { handler: async () => ({ body: 'ok' }) } },
+  '/user/privacy-policy': { POST: { handler: async () => ({ body: 'ok' }) } },
 };
 
 export default routes;
