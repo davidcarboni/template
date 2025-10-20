49deb01dbc1e8f624d3f6ba37ab7df88e496c361
David Carboni <david@carboni.io>
2025-10-20 22:16:06 +0100

    Simplify

--- api/src/routes/ping.ts
+++ api/src/routes/ping.ts
@@ -3,7 +3,6 @@ import { Request, Response } from '@scloud/lambda-api/dist/types';
 export async function ping(request: Request): Promise<Response> {
   console.log('ping', request.query);
   return {
-    statusCode: 200,
     body: { message: 'pong' },
   };
 }
